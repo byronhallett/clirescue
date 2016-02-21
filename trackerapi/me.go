9ab08e4fa4c28dd464f3bfd7bc5d57b90b05e85e
Byron Hallett <byron.james.h@gmail.com>
2016-02-21 15:27:05 +1100

    forking repo requires internal code changes

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/byronhallett/clirescue/cmdutil"
+	"github.com/byronhallett/clirescue/user"
 )
 
 var (
